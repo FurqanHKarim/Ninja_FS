3a772e3f75c7263d42567114bb1477e5a6710c05
FurqanHKarim <furqanhameedkarim63@gmail.com>
2023-07-27 15:21:34 +0500

    FT. MetaHeaderData, Chrono

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <chrono>    /* time_t, struct tm, difftime, time, mktime */
+#include <stdio.h>      /* printf */
+#include <sstream> // stringstream
+#include <iomanip> // put_time
+
+
+#include "Data.h"
+#include "functions.h"
+
+
+using namespace std;
+using namespace chrono;
+
+
+
+int main()
+{
+	cout << sizeof(MetaHeader);
+}
+
