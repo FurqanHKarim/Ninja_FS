b3d2e22bb1ec043d8834e630b77df3005b369545
WamiqSohail <wamiqsohail1998@gmail.com>
2023-08-02 17:34:39 +0500

    First Commit feat. Necessary Functions

--- include/Data.h
+++ include/Data.h
@@ -1,15 +1,20 @@
+#ifndef _DATAHEADER
+#define _DATAHEADER
+
 #include <stdint.h>
 #include<ctime>
 #include <chrono>
 using namespace std;
 
-struct uint128_t {
+struct uint128_t 
+{
 	uint64_t Delim1;
 	uint64_t Delim2;
 };
 
-struct MetaHeader {
-	uint128_t Delimit;                  //16 till here
+struct MetaHeader 
+{
+	uint128_t Delimit;                  //16 till here ///RECHECK
 	uint64_t  Creation_time;            //24 till here | diff8
 	uint64_t  Last_access_time;         //32 till here | diff8
 	uint64_t  Available_space;          //40 till here | diff8
@@ -18,10 +23,14 @@ struct MetaHeader {
 	uint8_t   Max_size;                 //56 till here | diff0 || padding of 6 bytes added 
 	char	  left_over_space[456];		//512 till here
 
-	MetaHeader();
+	//MetaHeader();
+	MetaHeader()
+	{
+
+	}
 }; 
 
 
-
+#endif
 
 
--- src/Data.cpp
+++ src/Data.cpp
@@ -2,10 +2,10 @@
 #include "functions.h"
 
 
-
-MetaHeader::MetaHeader() {
-	//Delimit = 0xDEADBEEFBEEFFEED;
-	auto temp = std::chrono::system_clock::now();
-	Creation_time = get_time();
-
-}
+//
+//MetaHeader::MetaHeader() {
+//	//Delimit = 0xDEADBEEFBEEFFEED;
+//	auto temp = std::chrono::system_clock::now();
+//	Creation_time = get_time();
+//
+//}
